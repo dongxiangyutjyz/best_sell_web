7f94806b34e596773a6628dd2080205657536026
James <xdong2@nd.edu>
2017-10-03 20:40:52 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var app = express();
+const cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
+const keys = require('./config/keys');
+const passport = require('passport');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 *60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+var User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send(req.body);
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server listening on port " + PORT);
+});
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:'402904865547-p1877usppql16f9cdpu7rojf22qu766k.apps.googleusercontent.com',
+  googleClientSecret: '_emUqsu0LZafeUjs-Uma7Gqk',
+  mongoURI:'mongodb://xdong2:James__1998@ds141534.mlab.com:41534/xdong2_emaily_dev',
+  cookieKey: 'ihgladkljfaoiedklxkcjaodisnv'
+};
